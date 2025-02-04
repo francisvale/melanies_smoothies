216b5832d12ecb41680b3b2c83cb0e309208369c
Francis Vale <79495688+francisvale@users.noreply.github.com>
2025-02-04 15:10:35 +0800

    Update streamlit_app.py
    instead of json put it in a table form

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 # Write directly to the app
 st.title("🥤 Customize Your Smoothie 🥤")
